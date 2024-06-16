034cd91efb766ef7d12de5ac9d8dddb15199ada3
Dimas <dimasriat@gmail.com>
2024-06-16 11:44:40 +0700

    feat: extract-method

---
+++ refactorings/techniques/composing-methods/extract-method.ts
@@ -0,0 +1,55 @@
+class MyClassBefore {
+  private name = 'Dimas';
+  private amount = 69420;
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getOutstanding(): number {
+    return this.amount;
+  }
+
+  public printBanner(): void {
+    console.log('THIS IS A BANNER');
+  }
+
+  // TODO(dims): NEED TO BE REFACTORED
+  public printOwing(): void {
+    this.printBanner();
+
+    // print details
+    console.log('Name:', this.getName());
+    console.log('Amount:', this.getOutstanding());
+  }
+}
+
+class MyClassAfter {
+  private name = 'Dimas';
+  private amount = 69420;
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getOutstanding(): number {
+    return this.amount;
+  }
+
+  public printBanner(): void {
+    console.log('THIS IS A BANNER');
+  }
+
+  // REFACTORED(dims): "Extract Method"
+  public printOwing(): void {
+    this.printBanner();
+
+    // print details
+    this.printDetails();
+  }
+
+  private printDetails(): void {
+    console.log('Name:', this.getName());
+    console.log('Amount:', this.getOutstanding());
+  }
+}
